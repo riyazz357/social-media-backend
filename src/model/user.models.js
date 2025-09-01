e4b53d21dbc69f0467fddd8155a68182c6d0e1a4
mohammad riyazuddin <mohammadriyazuddin494@gmail.com>
2025-09-01 15:30:54 +0530

    added the user controller and hashed the password

---
+++ src/model/user.models.js
@@ -0,0 +1,59 @@
+import mongoose from "mongoose";
+
+const userSchema= new mongoose.Schema({
+    firstName:{
+        type:String,
+        required:true,
+        lowercase:true,
+        trim:true
+    },
+    lastName:{
+        type:String,
+        required:true,
+        lowercase:true,
+        trim:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        trim:true,
+        lowercase:true
+    },
+    password:{
+        type:String,
+        required:true,
+    },
+    bio:{
+        type:String,
+        maxLength:250,
+    },
+    friends:{
+        type:[
+            {
+                type:mongoose.Schema.Types.ObjectId,
+                ref:'Users',
+            }
+        ],
+        default:[]
+    },
+    profilePicture: {
+        type: String, // URL to the image
+        default: 'default_avatar_url' // A link to a default placeholder image
+    },
+    coverPhoto: {
+        type: String, // URL to the image
+        default: 'default_cover_url'
+
+    }
+
+},{timestamps:true})
+
+userSchema.pre("save", async function(next){
+
+    if(!this.isModified('password')) return next();
+    this.password=await bcrypt.hash(this.password,10);
+    next();
+});
+
+export const User= mongoose.model("User",userSchema);
