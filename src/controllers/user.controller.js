40fd07b9170e0c131ac391b6dc5911654ed64f30
mohammad riyazuddin <mohammadriyazuddin494@gmail.com>
2025-09-05 16:37:56 +0530

    created the registeruser controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,45 +1,38 @@
-import {User} from '../model/user.models.js';
 
-const regitserUser= async(req,res)=>{
-    try{
-        const {firstName,lastName,email,password}=req.body
+import { User } from '../models/user.model.js'; 
+import { asyncHandler } from '../utils/asyncHandler.js'; 
 
-    if(!firstName || !lastName ||!email || !password){
-        return res
-        .status(401)
-        .json({message:"all fileds are required",error:error.message})
+const registerUser = asyncHandler(async (req, res) => {
+    //Get user details from the request body
+    const { firstName, lastName, email, password } = req.body;
+
+    //Check if any required field is missing or just an empty string
+    if ([firstName, lastName, email, password].some((field) => !field || field.trim() === "")) {
+        return res.status(400).json({ message: "All fields are required" });
     }
 
-    const existingUser= await User.findOne({email})
-    if(existingUser){
-        return res
-        .status(402)
-        .json({message:"user already exist!!!",error:error.message})
+    //Check if the user already exists
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+        return res.status(409).json({ message: "User with this email already exists" });
     }
 
-    const user= await User.create({
+    //Create the new user. The password will be hashed by the pre-save hook.
+    const user = await User.create({
         firstName,
         lastName,
         email,
         password,
+    });
 
-    })
+    //Efficiently get the created user object without the password
+    const createdUser = user.toObject();
+    delete createdUser.password;
 
-  user=await User.findById({_id}).select(
-    "-password"
-  )
+    return res.status(201).json({
+        message: "User registered successfully",
+        user: createdUser
+    });
+});
 
-  return res
-  .status(200)
-  .json({message:"user reigstered successfully",user})
-
-    }
-    catch(error){
-        return res
-        .status(500)
-        .json({message:"error while registering the user",error:error.message})
-    }
-    
-}
-
-export {regitserUser}
+export { registerUser };
